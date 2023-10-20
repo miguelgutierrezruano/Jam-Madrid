8437b4c3662d97b660d2858dac3ea8cd029399b8
Jesús García <jegagu10@gmail.com>
2023-10-20 20:01:22 +0200

    grid

---
+++ Assets/Scripts/GridGizmo.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridGizmo : MonoBehaviour
+{
+    public int gridWidth = 5;
+    public int gridHeight = 5; 
+    public float spacing = 1.0f; 
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position, Vector3.one);
+
+        for (int x = -gridWidth; x < gridWidth; x++)
+        {
+            for (int y = 0; y < gridHeight; y++)
+            {
+                Vector3 target = transform.position + new Vector3(0, y * spacing, x * spacing);
+
+                Gizmos.DrawWireCube(target, Vector3.one);
+            }
+        }
+    }
+}
